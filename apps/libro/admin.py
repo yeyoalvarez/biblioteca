5881d0ef6dde2d0c8689084fe5695ec924f39c9d
yeyoalvarez <gregorgg23@gmail.com>
2022-03-10 17:10:21 -0300

    barra de busqueda creada

--- apps/libro/admin.py
+++ apps/libro/admin.py
@@ -5,6 +5,9 @@ from .models import *
 # al colocar los modelos aqui luego se ven en el administrador
 #barra de busqueda
 
+class buscadoarLibros(admin.ModelAdmin):
+    search_fields = ['titulo', 'autor', 'lugar_impresion']
+    list_display = ['autor', 'edicion', 'coleccion']
 
-admin.site.register(libro)
+admin.site.register(libro, buscadoarLibros)
 admin.site.register(archivo)
